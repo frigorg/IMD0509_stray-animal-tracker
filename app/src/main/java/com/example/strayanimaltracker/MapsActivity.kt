1aafd2a4d11b433c1df56a5635d3343009d00549
frigorg <frigo.rg@gmail.com>
2019-11-17 03:33:54 -0300

    Sincronização dos métodos de posicionamento da cam

--- app/src/main/java/com/example/strayanimaltracker/MapsActivity.kt
+++ app/src/main/java/com/example/strayanimaltracker/MapsActivity.kt
@@ -24,6 +24,9 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_maps.*
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -35,52 +38,43 @@ class MapsActivity : AppCompatActivity(),
     GoogleMap.OnMyLocationClickListener {
 
     private val LOGTAG = "LOGTAG"
+    private val CAMERA_REQUEST_CODE = 1
 
     private lateinit var mMap: GoogleMap
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+    private var currentPhotoPath: String? = ""
     private lateinit var lastCoordinates: LatLng
+
+    private lateinit var currentUserId: String
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var storage = FirebaseStorage.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        currentUserId = auth.currentUser!!.uid
 
 
-        abrirCamera()
-
-
-
-
-        getLocation()
-        try {
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastCoordinates, 15F))
-        }catch (e :Exception) {
-            Log.e(LOGTAG,"Erro ao utilizar coordenadas.")
-            e.printStackTrace()
-        }
+        //abrirCamera()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         mMap.isMyLocationEnabled = true
         mMap.setOnMyLocationButtonClickListener(this)
         mMap.setOnMyLocationClickListener(this)
+
+        getLocation()
     }
 
     override fun onMyLocationButtonClick(): Boolean {
@@ -94,52 +88,33 @@ class MapsActivity : AppCompatActivity(),
     fun getLocation() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                val lat = location!!.latitude
-                val lng = location.longitude
-                lastCoordinates = LatLng(lat, lng)
-            }
-            .addOnFailureListener {
-                Toast.makeText(this, "Erro ao pegar a posição.", Toast.LENGTH_SHORT).show()
-            }
-    }
+        fusedLocationClient.lastLocation.addOnSuccessListener {location ->
+            val lat = location!!.latitude
+            val lng = location.longitude
+            lastCoordinates = LatLng(lat, lng)
 
-    private lateinit var auth: FirebaseAuth
+            try {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastCoordinates, 15F))
+            }catch (e :Exception) {
+                Log.e(LOGTAG,"Erro ao utilizar coordenadas.")
+                e.printStackTrace()
+            }
+
+        }.addOnFailureListener {
+            Log.e(LOGTAG,"Erro ao pegar a posição.")
+        }
+    }
 
     private fun logout() {
         auth.signOut()
         finish()
     }
 
-
-
-
-
-    val CAMERA_REQUEST_CODE = 1
-
     fun abrirCamera() {
         val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (callCameraIntent.resolveActivity(packageManager) != null) {
 
-            val photoFile: File? = try {
-                createImageFile()
-            } catch (e: IOException) {
-                Log.e(LOGTAG, e.message!!)
-                null
-            }
-
-
-            photoFile?.also {
-                val photoURI: Uri = FileProvider.getUriForFile(
-                    this,
-                    "com.example.strayanimaltracker.fileprovider",
-                    it
-                )
-                callCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
-            }
-
+            startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
         }
     }
 
@@ -150,13 +125,7 @@ class MapsActivity : AppCompatActivity(),
                 if(resultCode == Activity.RESULT_OK && data != null){
                     val imageBitmap = data.extras!!.get("data") as Bitmap
 
-
-                    //Colocar na galeria
-                    Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
-                        val f = File(currentPhotoPath!!)
-                        mediaScanIntent.data = Uri.fromFile(f)
-                        sendBroadcast(mediaScanIntent)
-                    }
+                    armazenarFoto(currentUserId, "teste1", imageBitmap)
                 }
             }
             else ->{
@@ -165,23 +134,23 @@ class MapsActivity : AppCompatActivity(),
         }
     }
 
-    private var currentPhotoPath: String? = ""
+    fun armazenarFoto(userId: String, postId: String, image: Bitmap) {
+        val storageRef = storage.reference
+        val imagesRef = storageRef.child("images")
+        val userRef = imagesRef.child(userId)
+        val fileName = "${postId}.jpg"
+        val fileRef = userRef.child(fileName)
 
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        val uploadTask = fileRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            Toast.makeText(this, "Falha durante o upload da imagem.", Toast.LENGTH_LONG).show()
+        }.addOnSuccessListener {
+            Toast.makeText(this, "Upload de imagem concluído.", Toast.LENGTH_LONG).show()
         }
     }
-
-
 
 }
